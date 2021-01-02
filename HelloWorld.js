496af1448442cc16d46a957111f1b2b84582e812
Aaluu10 <76809577+Aaluu10@users.noreply.github.com>
2021-01-02 07:18:39 +0500

    Add files via upload

---
+++ HelloWorld.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.end('Hello World');
+}).listen(9001);
+
+
+console.log('Server is running at port 9000');
